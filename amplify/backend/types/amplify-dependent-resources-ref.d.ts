6bbe6b0c8a54392a90fc8828606acb82fc04e8e4
shireen028 <shireen@gleecus.com>
2022-10-11 18:13:30 +0530

    Initial Commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "demo": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "demo": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Amplify } from "aws-amplify";
+import config from "./aws-exports";
+// src/index.js
+import { AmplifyProvider } from "@aws-amplify/ui-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "@aws-amplify/ui-react/styles.css";
+import { studioTheme } from "./ui-components";
+
+Amplify.configure(config);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AmplifyProvider theme={studioTheme}>
+      <App />
+    </AmplifyProvider>
   </React.StrictMode>
 );
 
